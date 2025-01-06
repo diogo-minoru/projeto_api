1b4a31ec708652424213f1ade2830e166fd63018
diogo-minoru <132161289+diogo-minoru@users.noreply.github.com>
2025-01-06 20:05:31 -0300

    adicionando api bitcoin

--- api.py
+++ api.py
@@ -1,22 +1,40 @@
 import requests
 import json
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
+from tinydb import TinyDB
+import pprint
+from datetime import datetime
 #pip install python-dotenv
-url = "https://api.openai.com/v1/chat/completions"
 
-open_ai_key = os.getenv("key")
+def extract():
+    url = "https://api.coinbase.com/v2/prices/spot"
+    response = requests.get(url)
+    return response.json()["data"]
 
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {open_ai_key}"
-}
 
-data = {
-    "model": "gpt-3.5-turbo",
-    "messages": [{"role": "user", "content": "Qual a capital do brasil?"}]
-}
+def transform(dados):
+    valor = dados["amount"]
+    criptomoeda = dados["base"]
+    moeda = dados["currency"]
+    timestamp = datetime.now().timestamp()
 
-response = requests.post(url = url, headers = headers, data = json.dumps(data))
+    dados_transformados = {
+        "valor": valor,
+        "criptomoeda": criptomoeda,
+        "moeda": moeda,
+        "timestamp": timestamp
+    }
 
-print(response.json())
+    return dados_transformados
+
+def salvar_dados_tiny_db(dados, db_name = "bitcoin.json"):
+    db = TinyDB(db_name)
+    db.insert(dados)
+    print("Dados salvos com sucesso!")
+
+if __name__ == "__main__":
+    dados_json = extract()
+    dados_tratados = transform(dados_json)
+    salvar_dados_tiny_db(dados_tratados)
+    #print(dados_tratados)
